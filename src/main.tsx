6c21cbe7e9b0c658029e54d8d6e6c3dfeb1f4671
jozef <jozefpv99@gmail.com>
2025-06-07 22:46:35 +0100

    prod config

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.tsx';
 import { AuthProvider } from './context/AuthContext.tsx';
 import { CampaignProvider } from './context/CampaignContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/clipeame">
     <AuthProvider>
       <CampaignProvider>
         <App />
       </CampaignProvider>
     </AuthProvider>
   </BrowserRouter>,
-)
+);
